7180b356e5052d0781a2cc28b7b79d4afe582e03
Neelam Veera Venkata Durga Sai Ram <83587898+sairam-neelam@users.noreply.github.com>
2021-09-13 22:07:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
-import {BrowserRouter, Route} from 'react-router-dom'
+import './App.css'
 
-import SignUp from './pages/SignUP'
-import BasicInfo from './pages/BasicInfo/BasicInfo'
 import DataList from './pages/DataList/DataList'
 
-const App = () => (
-  <BrowserRouter>
-    <Route exact path="/" component={SignUp} />
-    <Route exact path="/signUp/:email" component={BasicInfo} />
-    <Route exact path="/userList" component={DataList} />
-  </BrowserRouter>
-)
+const App = () => <DataList />
 export default App
